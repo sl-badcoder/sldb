4875fda5f5bec20db134a3fb77a312531612cbb4
sl-badcoder <senadlg01@gmail.com>
2025-06-25 22:54:09 +0200

    implemented simple erase and allocate logic

--- src/storage/sp_segment.cpp
+++ src/storage/sp_segment.cpp
@@ -3,6 +3,16 @@
 //
 #include "sp_segment.h"
 
-void SP_SEGMENT::allocate(uint16_t data_size){
+void SP_SEGMENT::erase(TID tid)
+{
+    auto& frame = buffer_pool_manager.getPageTable()[tid.page_id];
+    frame->getPage()->erase(tid.slot_id);
+}
 
+void SP_SEGMENT::allocate(uint16_t data_size)
+{
+    // find free page
+    page_id_t page_id = 0;
+    auto& frame = buffer_pool_manager.getPageTable()[page_id];
+    frame->getPage()->allocate(data_size);
 }
--- src/storage/sp_segment.h
+++ src/storage/sp_segment.h
@@ -6,11 +6,15 @@
 #define SP_SEGMENT_H
 
 #include <cstdint>
+#include <database/buffermanager/buffer_manager.h>
+
 #include "slotted_page.h"
 
 struct SP_SEGMENT{
+    SP_SEGMENT(BufferPoolManager& buffer_pool_manager):buffer_pool_manager(buffer_pool_manager){};
     void allocate(uint16_t data_size);
     void erase(TID tid);
+    BufferPoolManager& buffer_pool_manager;
     uint64_t segment_id;
 };
 
